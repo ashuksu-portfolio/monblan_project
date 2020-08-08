fd2fca3af622fa0acbc045d15bd6b831fbf57b0a
ashuksu-portfolio <ashuksu1@gmail.com>
2020-08-08 13:51:49 +0300

    start section product, added filter in section product

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,24 @@
 $(function () {
 
+   $('.icon-th-list').on('click', function () {
+      $('.product__item').addClass('product__item--list');
+      $('.icon-th-list').addClass('product__filter-btn--active');
+      $('.icon-th').removeClass('product__filter-btn--active');
 
-   $(".footer-copy").addClass("wow fadeInDown");
+   });
 
+
+   $('.icon-th').on('click', function () {
+      $('.product__item').removeClass('product__item--list');
+      $('.icon-th').addClass('product__filter-btn--active');
+      $('.icon-th-list').removeClass('product__filter-btn--active');
+   });
 
    // * --------------------------------------------------------
 
+   $(".footer-copy").addClass("wow fadeInDown");
 
    new WOW().init();
-
 
 
 });
