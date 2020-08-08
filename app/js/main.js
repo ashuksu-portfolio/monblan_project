6b554d41b0678568366a66dd94b44a65a3a8eaa9
ashuksu-portfolio <ashuksu1@gmail.com>
2020-08-09 01:49:14 +0300

    respons section product

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,30 @@
 $(function () {
 
-   $('.product__filter-btn--large').on('click', function () {
-      $('.product__item').addClass('product__item--large');
-      $('.product__filter-btn--large').addClass('product__filter-btn--active');
-      $('.product__filter-btn--list').removeClass('product__filter-btn--active');
-   });
-
-
    $('.product__filter-btn--list').on('click', function () {
-      $('.product__item').removeClass('product__item--large');
+      $('.product').addClass('product--list');
       $('.product__filter-btn--list').addClass('product__filter-btn--active');
       $('.product__filter-btn--large').removeClass('product__filter-btn--active');
    });
 
+   $('.product__filter-btn--large').on('click', function () {
+      $('.product').removeClass('product--list');
+      $('.product__filter-btn--large').addClass('product__filter-btn--active');
+      $('.product__filter-btn--list').removeClass('product__filter-btn--active');
+   });
+
    // * --------------------------------------------------------
 
-   // $(".footer-copy").addClass("wow fadeInDown");
+
+
+   // * --------------------------------------------------------
+
+   $(".product__item").addClass("wow flipInX");
+   $(".header__logo").addClass("wow fadeInLeft");
+   $(".header__box").addClass("wow fadeInDown");
+   $(".product__filter-btn").addClass("wow fadeInLeftBig");
+   $(".product__link").addClass("wow headShake");
+
 
    new WOW().init();
-
 
 });
