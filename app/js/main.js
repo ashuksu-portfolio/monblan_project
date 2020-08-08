7f27b313e2dd14986bb1d8ab1c508cc5a0a2f6cc
ashuksu-portfolio <ashuksu1@gmail.com>
2020-08-08 16:26:20 +0300

    create section product: product__item (list)

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,21 @@
 $(function () {
 
-   $('.icon-th-list').on('click', function () {
-      $('.product__item').addClass('product__item--list');
-      $('.icon-th-list').addClass('product__filter-btn--active');
-      $('.icon-th').removeClass('product__filter-btn--active');
-
+   $('.product__filter-btn--large').on('click', function () {
+      $('.product__item').addClass('product__item--large');
+      $('.product__filter-btn--large').addClass('product__filter-btn--active');
+      $('.product__filter-btn--list').removeClass('product__filter-btn--active');
    });
 
 
-   $('.icon-th').on('click', function () {
-      $('.product__item').removeClass('product__item--list');
-      $('.icon-th').addClass('product__filter-btn--active');
-      $('.icon-th-list').removeClass('product__filter-btn--active');
+   $('.product__filter-btn--list').on('click', function () {
+      $('.product__item').removeClass('product__item--large');
+      $('.product__filter-btn--list').addClass('product__filter-btn--active');
+      $('.product__filter-btn--large').removeClass('product__filter-btn--active');
    });
 
    // * --------------------------------------------------------
 
-   $(".footer-copy").addClass("wow fadeInDown");
+   // $(".footer-copy").addClass("wow fadeInDown");
 
    new WOW().init();
 
