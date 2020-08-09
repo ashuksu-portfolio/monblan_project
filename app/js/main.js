a42cc7bdfc577edf1d8e6bf4bab9687aed776146
ashuksu-portfolio <ashuksu1@gmail.com>
2020-08-09 05:26:47 +0300

    finish project

--- app/js/main.js
+++ app/js/main.js
@@ -14,13 +14,43 @@ $(function () {
 
    // * --------------------------------------------------------
 
+   // $('input, select').styler();
+
+   $('input').datepicker(
+      {
+         // inline: true,
+         // clearButton: true,
+
+
+
+
+         language: {
+            days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            today: 'Today',
+            clear: 'Clear',
+            dateFormat: 'mm-dd-yyyy',
+            timeFormat: 'hh:ii aa',
+            firstDay: 0
+
+
+         }
+      }
+   )
+
+
+
+   // $('input').data('datepicker')
 
 
    // * --------------------------------------------------------
 
    $(".product__item").addClass("wow flipInX");
    $(".header__logo").addClass("wow fadeInLeft");
-   $(".header__box").addClass("wow fadeInDown");
+   $(".header__box").addClass("wow fadeInRight");
    $(".product__filter-btn").addClass("wow fadeInLeftBig");
    $(".product__link").addClass("wow headShake");
 
